504f9a43fb08091a5c90376c8e8b552f9075f6c9
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-05-15 20:40:50 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/valberjunior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/valberjunior/hrpayroll/services/PaymentService.java
@@ -1,12 +1,36 @@
 package com.valberjunior.hrpayroll.services;
 
 import com.valberjunior.hrpayroll.entities.Payment;
+import com.valberjunior.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    private RestTemplate restTemplate;
+
+    public PaymentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public Payment getPayment(Long workerId, int days) {
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+
+        var worker = restTemplate
+                .getForObject(
+                        workerHost + "/workers/{id}",
+                        Worker.class,
+                        uriVariables
+                );
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
