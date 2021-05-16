025ab197960b5f3c0175140e08b5a3a8c0a1691a
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-05-15 22:06:03 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/valberjunior/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/valberjunior/hrpayroll/resources/PaymentResource.java
@@ -1,6 +1,7 @@
 package com.valberjunior.hrpayroll.resources;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.valberjunior.hrpayroll.entities.Payment;
 import com.valberjunior.hrpayroll.services.PaymentService;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +20,15 @@ public class PaymentResource {
         this.service = service;
     }
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         var payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Integer days) {
+        var payment = new Payment("Brann", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }
